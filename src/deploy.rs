e4820076bb9784bdd70c4eef41e0a32ab6b947ad
Kostis Karantias <kkarantias@gmail.com>
2019-08-18 16:19:04 +0300

    deploy: extract deployment function

--- src/deploy.rs
+++ src/deploy.rs
@@ -5,7 +5,21 @@ extern crate web3;
 use std::{fs, time};
 use web3::contract::{Contract, Options};
 use web3::futures::Future;
-use web3::types::U256;
+use web3::types::{Address, U256};
+use web3::Web3;
+
+fn deploy_contract<T: web3::Transport>(bin_file: &str, abi_file: &str, web3: Web3<T>, from_address: Address) -> Contract<T> {
+    let bytecode = fs::read_to_string(bin_file).unwrap();
+    return Contract::deploy(web3.eth(), &fs::read(abi_file).unwrap())
+        .unwrap()
+        .confirmations(0)
+        .poll_interval(time::Duration::from_secs(10))
+        .options(Options::with(|opt| opt.gas = Some(3_000_000.into())))
+        .execute(bytecode, (), from_address)
+        .unwrap()
+        .wait()
+        .unwrap()
+}
 
 pub fn run() {
     let (_eloop, transport) = web3::transports::Http::new("http://localhost:8545").unwrap();
@@ -16,19 +30,9 @@ pub fn run() {
     let balance = web3.eth().balance(accounts[0], None).wait().unwrap();
 
     println!("Balance: {}", balance);
-
-    // Get the contract bytecode for instance from Solidity compiler
-    let bytecode = fs::read_to_string("./contracts-output/SimpleStorage.bin").unwrap();
-    // Deploying a contract
-    let contract = Contract::deploy(web3.eth(), &fs::read("./contracts-output/SimpleStorage.abi").unwrap())
-        .unwrap()
-        .confirmations(0)
-        .poll_interval(time::Duration::from_secs(10))
-        .options(Options::with(|opt| opt.gas = Some(3_000_000.into())))
-        .execute(bytecode, (), accounts[0])
-        .unwrap()
-        .wait()
-        .unwrap();
+    let bin_file = "./contracts-output/SimpleStorage.bin";
+    let abi_file = "./contracts-output/SimpleStorage.abi";
+    let contract = deploy_contract(bin_file, abi_file, web3, accounts[0]);
 
     println!("{}", contract.address());
 
