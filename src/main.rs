9b775f2925039512297198f13c747bcb5d1bb1fd
Kostis Karantias <kkarantias@gmail.com>
2019-08-18 18:16:36 +0300

    create build directory if it doesn't exist

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::path::Path;
 use std::env;
+use std::fs;
 
 pub mod compiler;
 pub mod deploy;
@@ -9,7 +10,11 @@ fn compiler_main() {
     let contracts = compiler::get_contracts(Path::new(constants::CONTRACTS_PATH));
     println!("Compiling...");
     println!("{}", contracts.join("\n"));
-    let args = compiler::args(Path::new(constants::BUILD_PATH), &contracts);
+    let build_path = Path::new(constants::BUILD_PATH);
+    if !build_path.is_dir() {
+        fs::create_dir(build_path).expect("failed to create build directory");
+    }
+    let args = compiler::args(build_path, &contracts);
     compiler::run(&args);
 }
 
