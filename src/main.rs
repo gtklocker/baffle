21584ba497316f09dd6c84077f30327cdf12208f
Kostis Karantias <kkarantias@gmail.com>
2019-08-18 15:53:38 +0300

    Add hardcoded deployment method

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,31 @@
 use std::path::Path;
+use std::env;
 
 pub mod compiler;
+pub mod deploy;
 
-fn main() {
+fn compiler_main() {
     let contracts_path = Path::new("./contracts/");
     let contracts = compiler::get_contracts(contracts_path);
     println!("Compiling...");
     println!("{}", contracts.join("\n"));
     let args = compiler::args(&contracts);
     compiler::run(&args);
+}
+
+fn deploy_main() {
+    deploy::run();
+}
+
+fn main() {
+    for argument in env::args() {
+        if argument == "compile" {
+            compiler_main();
+            break;
+        }
+        if argument == "deploy" {
+            deploy_main();
+            break;
+        }
+    }
 }
